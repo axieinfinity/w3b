0a359cbea4f48ca578772cdd77d7f53511ae225a
Trung Nguyen <me@trungfinity.com>
2020-01-09 03:53:45 +0700

    Validate hex length right inside hex utilities

--- web3r-types/src/internal/hex.rs
+++ web3r-types/src/internal/hex.rs
@@ -17,7 +17,8 @@ const HEX_CHARS: &'static [u8] = b"0123456789abcdef";
 /// assert_eq!(&to_hex(&[0, 0, 0xf], false), "0x00000f");
 /// assert_eq!(&to_hex(&[0x1], false), "0x01");
 /// ```
-pub fn to_hex(bytes: &[u8], skip_leading_zeros: bool) -> String {
+pub fn to_hex(bytes: impl AsRef<[u8]>, skip_leading_zeros: bool) -> String {
+    let bytes = bytes.as_ref();
     let mut index = 0;
 
     if skip_leading_zeros {
@@ -55,17 +56,28 @@ pub fn to_hex(bytes: &[u8], skip_leading_zeros: bool) -> String {
 pub enum HexError {
     MissingPrefix,
     InvalidChar { char: char, index: usize },
+    IncorrectLen { len: usize, expected: usize },
+    LenOverflow { len: usize, at_most: usize },
 }
 
 impl fmt::Debug for HexError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             HexError::MissingPrefix => write!(f, "missing 0x prefix"),
+
             HexError::InvalidChar { char, index } => write!(
                 f,
                 "invalid hexadecimal character {} at index {}",
                 char, index
             ),
+
+            HexError::IncorrectLen { len, expected } => {
+                write!(f, "incorrect length {}, expected {}", len, expected)
+            }
+
+            HexError::LenOverflow { len, at_most } => {
+                write!(f, "length overflow {}, expected at most {}", len, at_most)
+            }
         }
     }
 }
@@ -79,27 +91,76 @@ impl fmt::Display for HexError {
 
 impl Error for HexError {}
 
+pub enum ExpectedHexLen {
+    Exact(usize),
+    AtMost(usize),
+}
+
+impl ExpectedHexLen {
+    pub fn validate(&self, len: usize) -> Result<(), HexError> {
+        match self {
+            ExpectedHexLen::Exact(expected) => {
+                if len != *expected {
+                    return Err(HexError::IncorrectLen {
+                        len,
+                        expected: *expected,
+                    });
+                }
+            }
+
+            ExpectedHexLen::AtMost(at_most) => {
+                if len > *at_most {
+                    return Err(HexError::LenOverflow {
+                        len,
+                        at_most: *at_most,
+                    });
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for ExpectedHexLen {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ExpectedHexLen::Exact(len) => write!(f, "length of {}", len),
+            ExpectedHexLen::AtMost(len) => write!(f, "length of at most {}", len),
+        }
+    }
+}
+
 /// Converts a hexadecimal string to a `Vec` of bytes.
 ///
 /// ```rust
 /// use web3r_types::internal::hex::{from_hex, HexError};
 ///
-/// assert_eq!(from_hex("0xfff0f").unwrap(), vec![0xf, 0xff, 0xf]);
-/// assert_eq!(from_hex("0x37").unwrap(), vec![0x37]);
-/// assert_eq!(from_hex("0xf").unwrap(), vec![0xf]);
-/// assert_eq!(from_hex("0x1").unwrap(), vec![0x1]);
+/// assert_eq!(from_hex("0xfff0f", None).unwrap(), vec![0xf, 0xff, 0xf]);
+/// assert_eq!(from_hex("0x37", None).unwrap(), vec![0x37]);
+/// assert_eq!(from_hex("0xf", None).unwrap(), vec![0xf]);
+/// assert_eq!(from_hex("0x1", None).unwrap(), vec![0x1]);
 ///
-/// assert_eq!(from_hex("0x0fff0f").unwrap(), vec![0xf, 0xff, 0xf]);
-/// assert_eq!(from_hex("0x000037").unwrap(), vec![0x37]);
-/// assert_eq!(from_hex("0x00000f").unwrap(), vec![0xf]);
-/// assert_eq!(from_hex("0x01").unwrap(), vec![0x1]);
+/// assert_eq!(from_hex("0x0fff0f", None).unwrap(), vec![0xf, 0xff, 0xf]);
+/// assert_eq!(from_hex("0x000037", None).unwrap(), vec![0x37]);
+/// assert_eq!(from_hex("0x00000f", None).unwrap(), vec![0xf]);
+/// assert_eq!(from_hex("0x01", None).unwrap(), vec![0x1]);
 ///
-/// assert_eq!(from_hex("12345").unwrap_err(), HexError::MissingPrefix);
-/// assert_eq!(from_hex("0x001g").unwrap_err(), HexError::InvalidChar { char: 'g', index: 5 });
+/// assert_eq!(from_hex("12345", None).unwrap_err(), HexError::MissingPrefix);
+/// assert_eq!(from_hex("0x001g", None).unwrap_err(), HexError::InvalidChar { char: 'g', index: 5 });
 /// ```
-pub fn from_hex(hex: &str) -> Result<Vec<u8>, HexError> {
+pub fn from_hex<'a>(
+    hex: impl AsRef<str>,
+    expected_len: impl Into<Option<&'a ExpectedHexLen>>,
+) -> Result<Vec<u8>, HexError> {
+    let hex = hex.as_ref();
+
     if !hex.starts_with("0x") {
         return Err(HexError::MissingPrefix);
+    }
+
+    if let Some(expected_len) = expected_len.into() {
+        expected_len.validate(hex.len())?;
     }
 
     let mut out = Vec::with_capacity(hex.len() >> 1);
