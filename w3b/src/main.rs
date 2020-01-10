67c8f631e2f53c6801d8ef8a737552cb0ba2d8b8
Trung Nguyen <me@trungfinity.com>
2020-01-11 04:08:31 +0700

    Process returned values with correct types

--- w3b/src/main.rs
+++ w3b/src/main.rs
@@ -1,4 +1,5 @@
-use w3b_types::Address;
+use num_bigint::BigUint;
+use w3b_types::{Address, BlockId};
 
 mod error;
 mod json_rpc;
@@ -18,21 +19,31 @@ async fn main() -> Result<(), Error> {
     let provider = HttpProvider::new(env!("JSON_RPC_URI").to_owned());
     let web3 = Web3::new(provider);
 
-    web3.eth_block_number().await;
+    let block_number = web3.eth_block_number().await?;
 
-    web3.eth_transaction_count(
-        // vitalik.eth
-        Address::from_hex("0xd8da6bf26964af9d7eed9e03e53415d37aa96045").unwrap(),
-        Some(9255000.into()),
-    )
-    .await;
+    println!("{:?}", block_number);
 
-    web3.eth_balance(
-        // vitalik.eth
-        Address::from_hex("0xd8da6bf26964af9d7eed9e03e53415d37aa96045").unwrap(),
-        Some(9255000.into()),
-    )
-    .await;
+    let nonce: u64 = web3
+        .eth_transaction_count(
+            // vitalik.eth
+            Address::from_hex("0xd8da6bf26964af9d7eed9e03e53415d37aa96045").unwrap(),
+            Some(block_number.into()),
+        )
+        .await?
+        .into();
+
+    println!("{}", nonce);
+
+    let balance: BigUint = web3
+        .eth_balance(
+            // vitalik.eth
+            Address::from_hex("0xd8da6bf26964af9d7eed9e03e53415d37aa96045").unwrap(),
+            Some(BlockId::Latest),
+        )
+        .await?
+        .into();
+
+    println!("{}", balance);
 
     Ok(())
 }
--- w3b/src/web3.rs
+++ w3b/src/web3.rs
@@ -1,6 +1,7 @@
-use w3b_types::{Address, BlockId};
+use serde::de::DeserializeOwned;
+use w3b_types::{Address, BlockId, BlockNumber, Uint256, Uint64};
 
-use super::provider::Provider;
+use super::{error::Error, json_rpc::Response, provider::Provider};
 
 pub struct Web3<T: Provider> {
     provider: T,
@@ -13,47 +14,40 @@ impl<T: Provider> Web3<T> {
 }
 
 impl<T: Provider> Web3<T> {
-    pub async fn eth_block_number(&self) {
-        let response = self
-            .provider
-            .execute("eth_blockNumber", vec![])
-            .await
-            .unwrap();
-
-        println!("{:?}", response);
+    pub async fn eth_block_number(&self) -> Result<BlockNumber, Error> {
+        self.execute("eth_blockNumber", vec![]).await
     }
 
     pub async fn eth_balance(
         &self,
         address: impl Into<Address>,
         block_id: impl Into<Option<BlockId>>,
-    ) {
+    ) -> Result<Uint256, Error> {
         let address = serde_json::to_value(address.into()).unwrap();
         let block_id = serde_json::to_value(block_id.into().unwrap_or_default()).unwrap();
-
-        let response = self
-            .provider
-            .execute("eth_getBalance", vec![address, block_id])
+        self.execute("eth_getBalance", vec![address, block_id])
             .await
-            .unwrap();
-
-        println!("{:?}", response);
     }
 
     pub async fn eth_transaction_count(
         &self,
         address: impl Into<Address>,
         block_id: impl Into<Option<BlockId>>,
-    ) {
+    ) -> Result<Uint64, Error> {
         let address = serde_json::to_value(address.into()).unwrap();
         let block_id = serde_json::to_value(block_id.into().unwrap_or_default()).unwrap();
-
-        let response = self
-            .provider
-            .execute("eth_getTransactionCount", vec![address, block_id])
+        self.execute("eth_getTransactionCount", vec![address, block_id])
             .await
-            .unwrap();
+    }
 
-        println!("{:?}", response);
+    async fn execute<U: DeserializeOwned>(
+        &self,
+        method: &str,
+        params: Vec<serde_json::Value>,
+    ) -> Result<U, Error> {
+        let value = self.provider.execute(method, params).await?;
+        let response: Response = serde_json::from_value(value)?;
+        let result = serde_json::from_value(response.result)?;
+        Ok(result)
     }
 }
